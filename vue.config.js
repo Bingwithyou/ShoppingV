d27390acc5a17ab36aefcf5a4fa90ccb10f760c6
Dracarys <muchtoosoon@outlook.com>
2023-01-19 16:22:19 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  // 关闭 eslint
+  lintOnSave: false,
+  // 代理跨域
+  devServer: {
+    proxy: {
+      // 只要是 api 开头的路径都转发给 taget 目标服务器
+      "/api": {
+        // 请求的服务器地址
+        target: "http://gmall-h5-api.atguigu.cn",
+      },
+    },
+  },
+});
